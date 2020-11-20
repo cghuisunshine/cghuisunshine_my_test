c322f491de1e11e59e7dd4dfb15ee85ad311b50d
tester <tester@heliyuntong.cn>
2020-11-20 10:26:24 +0800

    iteration one finished

---
+++ cghuisunshine_my_test/cghuisunshine_my_test/cghuisunshine_my_test.h
@@ -0,0 +1,18 @@
+//
+//  cghuisunshine_my_test.h
+//  cghuisunshine_my_test
+//
+//  Created by edz on 20.11.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for cghuisunshine_my_test.
+FOUNDATION_EXPORT double cghuisunshine_my_testVersionNumber;
+
+//! Project version string for cghuisunshine_my_test.
+FOUNDATION_EXPORT const unsigned char cghuisunshine_my_testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cghuisunshine_my_test/PublicHeader.h>
+
+
